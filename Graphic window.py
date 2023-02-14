bca138abbebae511602ab150ddbc517626c8830a
Dmitriy Slobodchuk <104769679+SlobodchukDmitriy3@users.noreply.github.com>
2023-02-14 23:00:45 +0200

    Add files via upload

---
+++ Graphic window.py
@@ -0,0 +1,35 @@
+from tkinter import*
+from tkinter.simpledialog import*
+Lbl=Label(text='Hello user')
+Lbl.pack()
+mas=[]
+def Klubnika():
+    for i in range(5):
+        a = int(askstring('1,2,3,4,5','mas['+str(i)+']='))
+    for i in range(5):
+        Lbox.insert(END,mas[i])
+from tkinter import*
+window=Tk()
+mas=[]
+Lbl=Label(text='Ввести массив')
+Lbl.pack()
+Ent=Entry()
+Ent.pack()
+Btn=Button(text='Вывести массив')
+Btn.pack()
+Lbox=Listbox()
+Lbox.pack()
+mas=[]
+def Malinka():
+    a = Ent.get()
+    mas = a.split()
+    for i in mas:
+        Lbox.insert(END,i)
+print('Hello user')
+
+
+
+
+
+
+                      
